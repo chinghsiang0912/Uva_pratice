0d32c502af2a6d2a15279e4af9ea611e883fce98
Hu_Ching_Hsiang <a3837a@yahoo.com.tw>
2016-01-12 19:43:43 +0800

    correct

--- one_star/272 - TEX Quotes/uva272.cpp
+++ one_star/272 - TEX Quotes/uva272.cpp
@@ -1,6 +1,21 @@
 #include "stdio.h"
-
+#include "stdlib.h"
 int main(void){
+    char s[500];
+    int c=0;
+    while(gets(s)){
+        for(int i=0;i<500;i++){
+            if(s[i]=='\0'){break;}
+            if(s[i]=='\"'){
+                c++;
+                if(c%2==1)printf("``");
+                else printf("''");
+            }
+            else
+                printf("%c",s[i]);
 
+        }
+        printf("\n");
+    }
     return 0;
 }
